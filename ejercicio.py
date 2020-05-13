ae9db8460d08675ac3027cbea306d38961c8662e
tricardocss80 <ricardonicolastasovac@gmail.com>
2020-05-13 00:23:32 -0300

    Hola

--- ejercicio.py
+++ ejercicio.py
@@ -1,17 +1,39 @@
+#Ingresar el nombre y apellido de un alumno y sus notas de matemática, literatura y física.
+#Se pide imprimir el nombre, el apellido y el promedio.
+#Si el promedio es mayor a 6 entonces debe aparecer un cartel que diga "Aprobado". Caso contrario, si tiene menos de 4
+# puntos imprimir "Insuficiente" y si tiene entre 4 y 5.99999 imprimir "A recuperatorio".
+#En caso de tener 9 puntos o más, imprimir (aparte del aprobado) "Alumno destacado".
 
-def ingresar_datos():
-    nombre = input("Ingrese su nombre por favor: ")
-    print("Su nombre es: " +nombre)
 
-    apellido = input("Ingrese su apellido por favor: ")
-    print("Su nombre es: " + apellido)
+def promedio_materias(matematicas,literatura,fisica):
+    return (matematicas + literatura + fisica)/3
 
-ingresar_datos()
 
-def ingresar_materias():
-    matematicas = int(input("Ingrese su nota de Matematicas por favor: "))
-    if matematicas <=10:
-       print("Usted ingreso : " +str(matematicas))
-    else:
-        print("Debe ingresar un valor entre 1 y 10")
-        return matematicas
+print("Bienvenido al sistema de calificaciones de la escuela")
+
+nombre = input("Ingrese su nombre por favor: ")
+apellido = input("Ingrese su apellido por favor: ")
+print("Como estas: " + nombre + apellido)
+
+matematicas = float(input("Ingrese su nota de Matematicas por favor: "))
+
+literatura = float(input("Ingrese su nota de Literatura por favor: "))
+
+fisica = float(input("Ingrese su nota de Fisica por favor: "))
+
+print("*****INFORME*****")
+
+promedio = promedio_materias(matematicas,literatura,fisica)
+print(promedio)
+
+if promedio > 6:
+    print("Aprobado")
+    if promedio >= 9:
+        print("Alumno destacado")
+
+elif 4 <= promedio <= 5.99999:
+    print("A recuperatorio")
+else:
+    print("Insuficiente")
+
+print("Gracias por utilizar el sistema de promedios de la escuela, que tenga buen dia...")
