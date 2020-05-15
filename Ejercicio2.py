f28d3f1f0dc7e2f33e898913001641ff1bbc1714
tricardocss80 <ricardonicolastasovac@gmail.com>
2020-05-15 13:28:18 -0300

    Modificacion ejercicio 1 y 2

---
+++ Ejercicio2.py
@@ -0,0 +1,21 @@
+print("Elige tu propio camino")
+
+inicio = input("Escribe empezar para comenzar el programa: ")
+
+while(inicio == 'empezar'):
+    print("""¿Que camino queres elegir?
+        Escribi la opcion deseada con un numero
+
+        1- Quiero saludar
+        2- Quiero multiplicar
+        3- Quiero salir del programa    """)
+    opcion = input("Ingresa una opcion: ")
+    if opcion == '1':
+        print("Hola como estas")
+    elif opcion == '2':
+        numero1 = float(input("Introduce un valor a multiplicar: "))
+        numero2 = float(input("Introduce un valor a multiplicar: "))
+        print("El resultado es: ", numero1 * numero2)
+    elif opcion == '3':
+        print("Excelente decision")
+        break
--- ejercicio.py
+++ ejercicio.py
@@ -1,38 +1,51 @@
-#Ingresar el nombre y apellido de un alumno y sus notas de matemática, literatura y física.
-#Se pide imprimir el nombre, el apellido y el promedio.
-#Si el promedio es mayor a 6 entonces debe aparecer un cartel que diga "Aprobado". Caso contrario, si tiene menos de 4
+# Ingresar el nombre y apellido de un alumno y sus notas de matemática, literatura y física.
+# Se pide imprimir el nombre, el apellido y el promedio.
+# Si el promedio es mayor a 6 entonces debe aparecer un cartel que diga "Aprobado". Caso contrario, si tiene menos de 4
 # puntos imprimir "Insuficiente" y si tiene entre 4 y 5.99999 imprimir "A recuperatorio".
-#En caso de tener 9 puntos o más, imprimir (aparte del aprobado) "Alumno destacado".
+# En caso de tener 9 puntos o más, imprimir (aparte del aprobado) "Alumno destacado".
 
-
-def promedio_materias(matematicas,literatura,fisica):
-    return (matematicas + literatura + fisica)/3
 
 print("Bienvenido al sistema de calificaciones de la escuela")
 
+
+def promedio_materias(matematica, literatura, fisica):
+    return (matematica + literatura + fisica) / 3
+
+
 nombre = input("Ingresa tu nombre por favor: ")
-apellido = input("Ingrese tu apellido por favor: ")
-print("Como estas: "+nombre, apellido)
+apellido = input("Ingresa tu apellido por favor: ")
+print("Es un gusto saludarte " + nombre, apellido)
 
-matematicas = float(input("Ingresa tu nota de Matematicas por favor: "))
 
-literatura = float(input("Ingresa tu nota de Literatura por favor: "))
+matematica = float(input("Ingresa por favor tu nota de Matematica: "))
+if matematica > 1 and matematica < 10:
+    print("Ingresaste: " + str(matematica))
+else:
+    print("Debes ingresar un numero entre 1 y 10")
 
-fisica = float(input("Ingresa tu nota de Fisica por favor: "))
 
-print("TU INFORME")
+literatura = float(input("Ingresa por favor tu nota de Literatura: "))
+if literatura > 1 and literatura < 10:
+    print("Ingresaste: " + str(matematica))
+else:
+    print("Debes ingresar un numero entre 1 y 10")
 
-promedio = promedio_materias(matematicas,literatura,fisica)
-print(promedio)
+fisica = float(input("Ingresa por favor tu nota de Fisica: "))
+print("Ingresaste: " + str(fisica))
+
+promedio = promedio_materias(matematica, literatura, fisica)
+
+print("Tu promedio es: " + str(promedio))
 
 if promedio > 6:
     print("Aprobado")
     if promedio >= 9:
         print("Alumno destacado")
 
-elif 4 <= promedio <= 5.99999:
+elif promedio >= 4 and promedio <= 5.99999:
     print("A recuperatorio")
+
 else:
     print("Insuficiente")
 
-print("Gracias por utilizar el sistema de promedios de la escuela, que tengas buen dia...")
+print("Gracias por utilizar el sistema de calificaciones de la escuela")
